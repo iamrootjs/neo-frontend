18993e486c598dd55b1ffd2b3cbf4104d4695dcd
iamrootjs <136985154+iamrootjs@users.noreply.github.com>
2025-10-09 05:47:20 +0200

    fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   modules: ["@nuxt/eslint", '@nuxtjs/tailwindcss', '@pinia/nuxt'],
   compatibilityDate: "2025-07-15",
